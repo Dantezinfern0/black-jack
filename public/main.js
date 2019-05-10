662db1b4c17b5bf2831b415acc741b2a8f1e7803
dantezinfern0 <danteharasz@gmail.com>
2019-05-10 08:37:57 -0400

    turn in

--- public/main.js
+++ public/main.js
@@ -1,7 +1,58 @@
+const ranks = [
+  { rank: 'A', value: 11 },
+  { rank: '2', value: 2 },
+  { rank: '3', value: 3 },
+  { rank: '4', value: 4 },
+  { rank: '5', value: 5 },
+  { rank: '6', value: 6 },
+  { rank: '7', value: 7 },
+  { rank: '8', value: 8 },
+  { rank: '9', value: 9 },
+  { rank: '10', value: 10 },
+  { rank: 'J', value: 10 },
+  { rank: 'Q', value: 10 },
+  { rank: 'K', value: 10 }
+]
+const suits = ['H', 'S', 'C', 'D']
+const fullDeck = []
+const playerHand = []
+const dealerHand = []
 const main = () => {
-  if (document.querySelector('h1.hello-world')) {
-    document.querySelector('h1.hello-world').textContent = 'Hello, World!'
+  for (let i = 0; i < ranks.length; i++) {
+    for (let j = 0; j < suits.length; j++) {
+      const card = {
+        rank: ranks[i].rank,
+        value: ranks[i].value,
+        suit: suits[j],
+        imgUrl: '/card-faces/' + ranks[i].rank + suits[j] + '.png'
+      }
+      fullDeck.push(card)
+      console.log('card pushed to deck')
+    }
+    console.log(fullDeck)
   }
 }
-
+const shuffle = () => {
+  for (let i = fullDeck.length - 1; i > 0; i--) {
+    const r = Math.floor(Math.random() * (i + 1))
+    const x = fullDeck[i]
+    fullDeck[i] = fullDeck[r]
+    fullDeck[r] = x
+  }
+  console.log(fullDeck)
+}
+// create below function for dealer hand
+const renderCard = () => {
+  for (let i = 0; i < 2; i++) {
+    // create an image tag
+    const imageTag = document.createElement('img')
+    // change img tag property src with the fullDeck[i].imgURL
+    imageTag.src = fullDeck[i].imgUrl
+    document.querySelector('.card-draw').appendChild(imageTag)
+    // pop these two cards to player hand
+    
+  }
+}
+document.querySelector('.draw-button').addEventListener('click', renderCard)
 document.addEventListener('DOMContentLoaded', main)
+document.addEventListener('DOMContentLoaded', shuffle)
