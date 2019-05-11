a23934d1158af0fb90b8047bd93045bd84281eda
dantezinfern0 <danteharasz@gmail.com>
2019-05-11 13:52:40 -0400

    fixed {} problems

--- public/main.js
+++ public/main.js
@@ -1,4 +1,4 @@
-import { setupMaster } from "cluster";
+import { setupMaster } from 'cluster'
 
 const ranks = [
   { rank: 'A', value: 11 },
@@ -16,12 +16,17 @@ const ranks = [
   { rank: 'K', value: 10 }
 ]
 const suits = ['H', 'S', 'C', 'D']
+// Deck array
 const fullDeck = []
+// arrays for drawn cards
 const playerHand = []
 const dealerHand = []
+// comparison variables
 let playerFinalScore = 0
 let dealerFinalScore = 0
-const resetGame = 0 //refresh page
+// reset by refresh
+const resetGame = location.reload(true)
+// function that creates card objects and puts them into deck array on page load
 const main = () => {
   for (let i = 0; i < ranks.length; i++) {
     for (let j = 0; j < suits.length; j++) {
@@ -37,6 +42,7 @@ const main = () => {
     console.log(fullDeck)
   }
 }
+// shuffles deck on page load
 const shuffle = () => {
   for (let i = fullDeck.length - 1; i > 0; i--) {
     const r = Math.floor(Math.random() * (i + 1))
@@ -49,67 +55,61 @@ const shuffle = () => {
 
 const playerScore = () => {
   let total = 0
-  for (i=0; i<playerHand.length; i++){
+  for (i = 0; i < playerHand.length; i++) {
     total += playerHand[i].value
     // sum of playersHand.value
     playerFinalScore.push(total)
-  } 
-  if (playerScore >21){
-    // bust! statement
-    document.querySelector('.player-message').textContent('BUST!! - SORRY, TRY AGAIN!')
   }
-    
-    
-const dealerScore = () => {
-      let total = 0
-      for (i=0; i<dealerHand.length; i++){
-        total += dealerHand[i].value
-        // sum of dealerHand.value
-        dealerFinalScore.push(total)
-      }
-      if (total > 21){
-        for (i=0; i< total.length; i++){
-          document.querySelector('.dealer-message').textContent('BUSTED! - PLAYER WINS!!')
-        } while (total < 17){
-        const dealerCard = fullDeck.pop()
-        const dealerImg = document.createElement('img')
-        dealerImg.src = dealerCard.imgUrl
-        document.querySelector('.dealer-hand').appendChild(dealerImg)
-        // next compare dealer score to see if dealer wins
-      } if (dealerScore > playerScore){
-          document.querySelector('.dealer-message').textContent('Dealer Wins!')
-          // now compare player score to see if  player wins
-      } else if (playerScore > dealerScore){
-        document.querySelector('.player-message').textContent('You Win!')
-        // now compare scores in the event of a tie or push
-      }else if (dealerScore === playerScore){
-        document.querySelector('.player-message').textContent('PUSH!')
-        document.querySelector('.dealer-message').textContent('PUSH!')
-      }
-      
-      
-      // function to draw player hand
-const dealOneCard = () => {
-        const dealtCard = fullDeck.pop()
-        const imageTag = document.createElement('img')
-        imageTag.src = dealtCard.imgUrl
-        document.querySelector('.card-draw').appendChild(imageTag)
-        playerHand.push(dealtCard)
-  console.log(dealtCard)
+  if (playerScore > 21) {
+    // bust! statement
+    document
+      .querySelector('.player-message')
+      .textContent('BUST!! - SORRY, TRY AGAIN!')
+  }
 }
 
-// function to draw dealer hand
-// create an image tag
-// change img tag property src with the fullDeck[i].imgURL
-// pop these two cards into player's hand array
+const dealerScore = () => {
+  let total = 0
+  for (i = 0; i < dealerHand.length; i++) {
+    total += dealerHand[i].value
+    // sum of dealerHand.value
+    dealerFinalScore.push(total)
+  }
+  if (total > 21) {
+    for (i = 0; i < total.length; i++) {
+      document
+        .querySelector('.dealer-message')
+        .textContent('BUSTED! - PLAYER WINS!!')
+    }
+    while (total < 17) {
+      const dealerCard = fullDeck.pop()
+      const dealerImg = document.createElement('img')
+      dealerImg.src = dealerCard.imgUrl
+      document.querySelector('.dealer-hand').appendChild(dealerImg)
+      // next compare dealer score to see if dealer wins
+    }
+    if (dealerScore > playerScore) {
+      document.querySelector('.dealer-message').textContent('Dealer Wins!')
+      // now compare player score to see if  player wins
+    } else if (playerScore > dealerScore) {
+      document.querySelector('.player-message').textContent('You Win!')
+      // now compare scores in the event of a tie or push
+    } else if (dealerScore === playerScore) {
+      document.querySelector('.player-message').textContent('PUSH!')
+      document.querySelector('.dealer-message').textContent('PUSH!')
+    }
+  }
+}
 
-// create the 'hit me' button function
-// adds one card and then calculates if there is a bust situation.
-// upon hitting the stay button, the dealer play loop will run
-// dealer play: if less than 17, hit - if above 17 evaluate winner
-// if above 21 bust
-// declare winner
-// reset button
+// function to draw player hand on at a time and evaluate score?
+const dealOneCard = () => {
+  const dealtCard = fullDeck.pop()
+  const imageTag = document.createElement('img')
+  imageTag.src = dealtCard.imgUrl
+  document.querySelector('.card-draw').appendChild(imageTag)
+  playerHand.push(dealtCard)
+  console.log(dealtCard)
+}
 document.querySelector('.stand-button').addEventListener('click', dealerScore)
 document.querySelector('.draw-button').addEventListener('click', dealOneCard)
 document.querySelector('.reset-button').addEventListener('click', resetGame)
