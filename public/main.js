1c0c76dce9ce33526c855535547c3b0ae7b08117
dantezinfern0 <danteharasz@gmail.com>
2019-05-14 18:39:30 -0400

    overhaul

--- public/main.js
+++ public/main.js
@@ -14,80 +14,94 @@ const ranks = [
   { rank: 'K', value: 10 }
 ]
 const suits = ['H', 'S', 'C', 'D']
-const fullDeck = []
-const playerHand = []
-const dealerHand = []
-let dTotal = 0
-let pTotal = 0
-const main = () => {
-  for (let i = 0; i < ranks.length; i++) {
-    for (let j = 0; j < suits.length; j++) {
-      const card = {
-        rank: ranks[i].rank,
-        value: ranks[i].value,
-        suit: suits[j],
-        imgUrl: '/card-faces/' + ranks[i].rank + suits[j] + '.png'
-      }
-      fullDeck.push(card)
-      console.log(card)
-    }
-    console.log(fullDeck)
+let dealerHand = []
+let playerHand = []
+let fullDeck = []
+let playerScore = 0
+let dealerScore = 0
+const playerHit = () => {
+  dealCardToPlayer()
+  if (playerScore > 21) {
+    document.querySelector('.player-message').textContent = 'BUSTED!'
+    disablePlayerActions()
+  } else {
+    dealCardToPlayer()
   }
 }
-const shuffle = () => {
+const playerStay = () => {
+  disablePlayerActions()
+  document.querySelector('.dealer-hand').textContent = ' '
+  for (let i = 0; i < dealerHand.length; i++) {
+    const card = dealerHand[i]
+    const img = document.creatElement('img')
+    img.src = card.imgUrl
+    document.querySelector('.dealer-hand').appendChild(img)
+  } while (dealerScore < 17) {
+    for (let j =0; j<dealerHand.length j++) {
+      dealerScore += dealerHand[j]
+  }
+}
+const disablePlayerActions = () => {
+  document.querySelector('.draw-button').disabled = true
+  document.querySelector('.stand-button').disabled = true
+}
+const createDeck = () => {
+  for (let _ = 0; (_ = 2); _++) {
+    for (let i = 0; i < ranks.length; i++) {
+      for (let j = 0; j < suits.length; j++) {
+        const card = {
+          rank: ranks[i].rank,
+          value: ranks[i].value,
+          suit: suits[j],
+          imgUrl: '/card-faces/' + ranks[i].rank + suits[j] + '.png'
+        }
+        fullDeck.push(card)
+      }
+    }
+  }
+}
+
+const shuffleDeck = () => {
   for (let i = fullDeck.length - 1; i > 0; i--) {
     const r = Math.floor(Math.random() * (i + 1))
     const x = fullDeck[i]
     fullDeck[i] = fullDeck[r]
     fullDeck[r] = x
   }
-  console.log(fullDeck)
 }
-const dealOneCard = () => {
+const dealCardToPlayer = () => {
   const dealtCard = fullDeck.pop()
-  const imageTag = document.createElement('img')
-  imageTag.src = dealtCard.imgUrl
-  document.querySelector('.card-draw').appendChild(imageTag)
-  pTotal.value += dealtCard.value
   playerHand.push(dealtCard)
-  if (playerHand.length > 1) {
-    document.querySelector('.stand-button').disabled = false
-  } else if (pTotal.value > 21) {
-    document.querySelector('.player-message').textContent = 'BUSTED'
-    document.querySelector('.draw-button').disabled = true
-  } else dealerScore()
+  const img = document.creatElement('img')
+  img.src = dealtCard.imgUrl
+  const li = document.createElement('li')
+  li.appendChild(img)
+  document.querySelector('.player-hand>ul').appendChild(li)
+  document.querySelector('.playerScore').textContent = playerScore
+  playerScore += dealtCard.value
+  document.querySelector('.player-score')
 }
-const dealerScore = () => {
-  document.querySelector('.stand-button').disabled = true
-  while (dTotal.value < 17) {
-    const dealerCard = fullDeck.pop()
-    const dealerImg = document.createElement('img')
-    dealerImg.src = dealerCard.imgUrl
-    document.querySelector('.dealer-hand').appendChild(dealerImg)
-    dealerHand.push(dealerCard)
-    for (let i = 0; i < dealerHand.length; i++) {
-      dTotal.value += dealerHand[i].value
-    }
-    calculateWinner()
-  }
+const dealCardToDealer = () => {
+  const dealtCard = fullDeck.pop()
+  playerHand.push(dealtCard)
+  const img = document.creatElement('img')
+  img.src = '/card-back/red_back.png'
+  const li = document.createElement('li')
+  li.appendChild(img)
+  document.querySelector('.dealer-hand>ul').appendChild(li)
+  document.querySelector('.dealerScore').textContent = dealerScore
+  dealerScore += dealtCard.value
+  document.querySelector('.dealer-score')
 }
-const calculateWinner = () => {
-  for (let i = 0; i <= dealerHand[i].length; i++) {
-    let dTotal = 0
-    dTotal += dealerHand[i].value
-    if (dTotal > 21) {
-      document.querySelector('.player-message').textContent =
-        'DEALER BUSTED! YOU WIN!!'
-    } else if (dTotal === pTotal) {
-      document.querySelector('.player-message').textContent = 'PUSH!'
-    } else if (dTotal > pTotal) {
-      document.querySelector('.player-message').textContent = 'Dealer Wins!'
-    } else parseInt(dTotal < pTotal)
-    document.querySelector('.player-message').textContent = 'You Win!'
-  }
+
+const main = () => {
+  createDeck()
+  shuffleDeck()
+  dealCardToPlayer()
+  dealCardToPlayer()
+  dealCardToDealer()
+  dealCardToDealer()
 }
-document.querySelector('.stand-button').disabled = true
-document.querySelector('.stand-button').addEventListener('click', dealerScore)
-document.querySelector('.draw-button').addEventListener('click', dealOneCard)
-document.addEventListener('DOMContentLoaded', main)
-document.addEventListener('DOMContentLoaded', shuffle)
+document.addEventListener('DOM').addEventListener(main)
+document.addEventListener('.draw-button').addEventListener('click', playerHit)
+document.addEventListener('.stand-button').addEventListener('click', playerStay)
